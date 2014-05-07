7d67412f5397c5ff1ba917d8686b83e13fc9ac93
mateus <mateus@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2014-05-07 05:28:31 +0000

    E1: Blocking Thread Synchronization - Solved.
    git-svn-id: https://epos.lisha.ufsc.br/svn/branches/ine5424@2906 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- include/thread.h
+++ include/thread.h
@@ -50,7 +50,7 @@ public:
 public:
     Thread(int (* entry)(), 
            const State & state = READY, const Priority & priority = NORMAL, unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this, priority)
+    : _state(state), _waiting(0), _link(this, priority)
     {
         lock();
 
@@ -63,7 +63,7 @@ public:
     template<typename T1>
     Thread(int (* entry)(T1 a1), T1 a1,
            const State & state = READY, const Priority & priority = NORMAL, unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this, priority)
+    : _state(state), _waiting(0), _link(this, priority)
     {
         lock();
 
@@ -76,7 +76,7 @@ public:
     template<typename T1, typename T2>
     Thread(int (* entry)(T1 a1, T2 a2), T1 a1, T2 a2,
            const State & state = READY, const Priority & priority = NORMAL, unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this, priority)
+    : _state(state), _waiting(0), _link(this, priority)
     {
         lock();
 
@@ -89,7 +89,7 @@ public:
     template<typename T1, typename T2, typename T3>
     Thread(int (* entry)(T1 a1, T2 a2, T3 a3), T1 a1, T2 a2, T3 a3,
            const State & state = READY, const Priority & priority = NORMAL, unsigned int stack_size = STACK_SIZE)
-    : _state(state), _link(this, priority)
+    : _state(state), _waiting(0), _link(this, priority)
     {
         lock();
 
@@ -122,6 +122,11 @@ protected:
 
     static void lock() { CPU::int_disable(); }
     static void unlock() { CPU::int_enable(); }
+    static bool locked() { return CPU::int_enabled(); }
+
+    static void sleep(Queue * q);
+    static void wakeup(Queue * q);
+    static void wakeup_all(Queue * q);
 
     static void reschedule();
 
@@ -146,6 +151,7 @@ protected:
     Log_Addr _stack;
     Context * volatile _context;
     volatile State _state;
+    Queue * _waiting;
     Queue::Element _link;
 
     static Scheduler_Timer * _timer;
