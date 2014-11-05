bbc5a52cf2a43ee1b5eb648f5ace4cafcf00c8a3
Tiago Royer <royertiago@gmail.com>
2014-11-05 17:22:05 -0200

    Started implementation of system calls; Added namespace mpl (MetaProgramming Library, name copied from Boost); Added metafunctions is_same and type_at_index and is_same; Ignoring files src/init/pc_init and src/system/pc_system.

---
+++ include/mpl.h
@@ -0,0 +1,49 @@
+/* mpl.h
+ * Metaprogramming library.
+ *
+ * All functions in this file are either value metafunctions or
+ * type metafunctions; that is, consists of a struct with either
+ * a static integer or boolean constant value named 'value',
+ * or a nested typedef named 'type', that correspond to the return
+ * value of the respective function.
+ *
+ * The naming conventions was chosen to resemble Boost.MPL.
+ *
+ * All metafunctions are in namespace mpl, outside namespace EPOS;
+ * this was done so the library could be reused with little or
+ * no code change.
+ */
+#ifndef __mpl_h
+#define __mpl_h
+
+namespace mpl {
+
+/* True if both types are the same, false if they are different.
+ * This template is sensible to cv-qualifications. */
+template< typename T, typename U >
+struct is_same {
+    static const bool value = false;
+};
+template< typename T >
+struct is_same<T, T> {
+    static const bool value = true;
+};
+
+/* Returns the Nth element of the list.
+ * The indexing is 0 based; that is,
+ * type_at_index< 1, int, double >::value is double. */
+template< int N, typename ... List >
+struct type_at_index;
+
+template< typename Head, typename ... Tail >
+struct type_at_index< 0, Head, Tail... > {
+    typedef Head type;
+};
+template< int N, typename Head, typename ... Tail >
+struct type_at_index< N, Head, Tail... > {
+    typedef typename type_at_index<N-1, Tail...>::type type;
+};
+
+} // namespace mpl
+
+#endif // __mpl_h
---
+++ src/abstraction/mpl_test.cc
@@ -0,0 +1,37 @@
+// Metaprogram library unit test
+#include <mpl.h>
+
+#define static_assert( ... ) static_assert( __VA_ARGS__ , #__VA_ARGS__ ) // ugly
+
+using namespace mpl;
+
+int main() {
+    static_assert( is_same<int, int>::value );
+    static_assert( is_same<signed, int>::value );
+    static_assert( is_same<const int, const int>::value );
+    static_assert( is_same<volatile char, volatile char>::value );
+    static_assert( !is_same<int, const int>::value );
+    static_assert( !is_same<volatile char, char>::value );
+    static_assert( !is_same<char, signed char>::value );
+    static_assert( !is_same<int, double>::value );
+
+    static_assert( is_same<
+            type_at_index<0, int, char, const char, double>::type,
+            int
+        >::value );
+    static_assert( is_same<
+            type_at_index<1, int, char, const char, double>::type,
+            char
+        >::value );
+    static_assert( is_same<
+            type_at_index<2, int, char, const char, double>::type,
+            const char
+        >::value );
+    static_assert( is_same<
+            type_at_index<3, int, char, const char, double>::type,
+            double
+        >::value );
+    static_assert( is_same< type_at_index<0, int>::type, int >::value );
+    static_assert( !is_same< type_at_index<0, unsigned>::type, int>::value );
+    return 0;
+}
