1552991550c702a3d5be92f449c064aaf44cb242
Tiago Royer <royertiago@gmail.com>
2014-12-09 00:30:10 -0200

    Replaced the constructors of Stub with variadic ones.

--- include/system/stub.h
+++ include/system/stub.h
@@ -12,15 +12,8 @@ template<typename Component, bool remote>
 class Stub: public Adapter<Component>
 {
 public:
-    Stub() {}
-    template<typename T1>
-    Stub(const T1 & a1): Adapter<Component>(a1) {}
-    template<typename T1, typename T2>
-    Stub(const T1 & a1, const T2 & a2): Adapter<Component>(a1, a2) {}
-    template<typename T1, typename T2, typename T3>
-    Stub(const T1 & a1, const T2 & a2, const T3 & a3): Adapter<Component>(a1, a2, a3) {}
-    template<typename T1, typename T2, typename T3, typename T4>
-    Stub(const T1 & a1, const T2 & a2, const T3 & a3, const T4 & a4): Adapter<Component>(a1, a2, a3, a4) {}
+    template< typename ... Ts >
+    Stub( const Ts & ... ts ) : Adapter<Component>(ts...) {}
 
     ~Stub() {}
 };
@@ -29,15 +22,8 @@ template<typename Component>
 class Stub<Component, true>: public Proxy<Component>
 {
 public:
-    Stub() {}
-    template<typename T1>
-    Stub(const T1 & a1): Proxy<Component>(a1) {}
-    template<typename T1, typename T2>
-    Stub(const T1 & a1, const T2 & a2): Proxy<Component>(a1, a2) {}
-    template<typename T1, typename T2, typename T3>
-    Stub(const T1 & a1, const T2 & a2, const T3 & a3): Proxy<Component>(a1, a2, a3) {}
-    template<typename T1, typename T2, typename T3, typename T4>
-    Stub(const T1 & a1, const T2 & a2, const T3 & a3, const T4 & a4): Proxy<Component>(a1, a2, a3, a4) {}
+    template< typename ... Ts >
+    Stub( const Ts & ... ts ) : Proxy<Component>( ts... ) {}
 
     ~Stub() {}
 };
